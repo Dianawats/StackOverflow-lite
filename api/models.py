57a55a4e77c795f8e74b772b154425a2dae12fce
Dianawats <dianakiznaki@gmail.com>
2018-08-15 02:43:56 +0300

    adding model module in OOP[Feature 159757288]

---
+++ api/models.py
@@ -0,0 +1,42 @@
+class Questions:
+    """Defines questions class"""
+
+    def list_of_question(self):
+        """
+        Initializes the questions object
+        :args None
+        :return Questions
+        """
+        self.questions = [
+            {
+                'id': 1,
+                'question_body': 'how to write a method using hibernate which returns boolean',
+                'question_tags': 'javascript', 
+                'answer': [
+                    {
+                        'answer_id': 1,
+                        'answer_body': 'for that debug and check what your query exactly returns if true then here also will return true only',
+
+                    }
+                ]
+            },
+            {
+                'id': 2,
+                'question_body': 'How is programming taking us',
+                'question_tags': 'import',
+                'answer': [
+                    {
+                        'answer_id': '',
+                        'answer_body': '',
+                    }
+                ]
+            }
+        ]
+
+        return self.questions
+
+    def __str__(self):
+        """String representation of the list returns
+        """
+        return str(self.list_of_question)
+
