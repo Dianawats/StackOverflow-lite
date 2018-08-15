09cc287702d2d2e42337ae3156203d4a960da1f7
Dianawats <dianakiznaki@gmail.com>
2018-08-15 13:18:51 +0300

    adding the user endpoint

--- api/models/user.py
+++ api/models/user.py
@@ -1,18 +1,19 @@
 import re
 from api.models.user import Users
 
-class User(object):
-    """Creating a class for the user model"""
 
-    def __init__(self, user_id, username, email, password):
+class User(object):
+    """Creating a class for the user model
+    """
+
+    def __init__(self, user_id, email, password):
         self.user_id = user_id
-        self.username = username
         self.email = email
         self.password = password
 
     @property
     def email(self):
-        return self.email
+        return self._email
 
     @email.setter
     def email(self, value):
@@ -22,6 +23,21 @@ class User(object):
             raise ValueError('Enter Valid Email ID forexample "sue@gmail.com"')
         self._email = value
 
+    @property
+    def password(self):
+        return self._password
 
-
-    
+    @password.setter
+    def password(self, pwd):
+        if not pwd:
+            raise Exception("Field can't be empty")
+        if len(pwd) < 8 and len(pwd) > 12:
+            raise Exception(
+                "Weak password \n Password must be 8 characters long ")
+        if not re.search(r'[0-9]', pwd):
+            raise Exception(
+                'Weak password \n Password should have atleast one integer')
+        if pwd.isupper() or pwd.islower() or pwd.isdigit():
+            print(
+                "Weak password \n Either you need to include alphabets or \n try include both letter cases")
+        self._password = pwd
