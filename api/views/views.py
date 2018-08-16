5884faf3e3471ece7fb5c82e16e693959859052a
Dianawats <dianakiznaki@gmail.com>
2018-08-16 05:39:04 +0300

    adding post by id method[Fixes #159757419]

--- api/views/views.py
+++ api/views/views.py
@@ -22,10 +22,6 @@ def index():
 
 
 @app.route('/api/v1/questions', methods=['GET'])
-"""Creating a get question function, that is\ 
-associated with the URI and only for GET http method
-and returns JSON data
-"""
 def get_questions():
     return jsonify({'questions': questions})
 
@@ -43,4 +39,23 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
+@app.route('/api/v1/questions', methods=[POST])
+def create_question():
+    if not request.json or not in 'title' in request.json:
+        abort(400)
+    question = {
+        'id': questions[-1]['id'] + 1
+        'title': request.json['title'],
+        'question_body': request.json.get('question_body', ""),
+    }
+    questions.append(question)
+    return jsonify({['question']}), 201
+
+
+@app.route('/api/v1/questions/<int:question_id>/answers', methods=['POST'])
+def get_question(question_id):
+    question = [question in questions if question['id'] == question_id]
+    if len(question) == 0:
+        abort(404)
+    return jsonify({'question': question[0]})
 
