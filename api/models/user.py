3395f2f6fb609cc6ea22326d5302eaa729362fab
Dianawats <dianakiznaki@gmail.com>
2018-08-16 11:02:43 +0300

    updating the user model

--- api/models/user.py
+++ api/models/user.py
@@ -3,17 +3,34 @@ from api.models.user import Users
 from api import generate_id
 
 users_list = []
-users_id = generate_id(users_list)
+user_id = generate_id(users_list)
 
 
 class User(object):
     """Creating a class for the user model
     """
 
-    def __init__(self, user_id, email, password):
+    def __init__(self, user_id, first_name, last_name, email, password):
         self.user_id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.password = password
+
+    @property
+    def first_name(self):
+        return self._first_name
+        
+    @first_name.setter
+    def first_name(self, value):
+        if not value:
+            raise Exception("the field can't be null")
+        if len(value) <= 2:
+            raise Exception("the first name is very short")
+        if re.compile('[!@#$%^&*:;?><.0-8]').match(value):
+            raise ValueError("not permitted xters are invalid")
+
+        self._first_name = value
 
     @property
     def email(self):
@@ -46,3 +63,5 @@ class User(object):
                 "Weak password \n Either you have to add alphabets or \
                  try to apply both letter cases")
         self._password = pwd
+
+      
