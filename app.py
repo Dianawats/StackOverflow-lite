de04a09258c4e1d46c567bf2ee2f77cc7ce65f79
Dianawats <dianakiznaki@gmail.com>
2018-08-18 09:47:21 +0300

    updating app to run[Fixes #159757288]

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from app import app
+import os
+from api import app
 
+from config import app_config
+
+config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
 
 if __name__ == '__main__':
     """creates application that runs"""
-    app.run(debug=True)
+
+    app.config.from_object(app_config.get(config_name))
+    app.run()
+
+
