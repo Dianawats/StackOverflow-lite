94c354c820e13a77656eb721b2ea6284d8b6656c
Dianawats <dianakiznaki@gmail.com>
2018-08-20 12:12:42 +0300

    modifying some tests and editing [Fixes #159824267]

--- tests/tests.py
+++ tests/tests.py
@@ -9,7 +9,6 @@ DEFAULT_URL = 'http://127.0.0.1:5000/api/v1/questions'
 GOOD_URL = '{}/2'.format(DEFAULT_URL)
 BAD_URL = '{}/3'.format(DEFAULT_URL)
 ANSWER_URL = '{}/2/answers'.format(DEFAULT_URL)
-NO_QUESTION_URL = '{}/3/answers'.format(DEFAULT_URL)
 
 
 class ApiTestCase(unittest.TestCase):
@@ -23,15 +22,22 @@ class ApiTestCase(unittest.TestCase):
         self.answerCopy = deepcopy(views.answers)
         self.app = app.app.test_client()
         self.app.testing = True
+
+    def test_api_can_get_all_questions(self):
+        """Test API can get a question (GET request)."""
+        res = self.app.get(DEFAULT_URL)
+        data = json.loads(res.get_data())
+        self.assertEqual(len(data['questions']), 2)
+        self.assertEqual(res.status_code, 200)
         
-    def test_get_single_question(self):
+    def test_api_get_single_question(self):
         res = self.app.get(DEFAULT_URL)
         data = json.loads(res.get_data())
         self.assertEqual(data['questions'][0]['question_title'], 
                          'Datatypes-boolean')
         self.assertEqual(res.status_code, 200)
 
-    def test_post_answer(self):
+    def test_api_can_post_answer(self):
         answer = {
             'answer_body': 'java is also an OOP programming language'
         }
@@ -40,38 +46,28 @@ class ApiTestCase(unittest.TestCase):
                             content_type='application/json')
         self.assertEqual(res.status_code, 201)
 
-    def test_add_question(self):
+    def test_api_can_add_question(self):
         question = {
-            'question_body': 'how to write a method using hibernate',
-            'question_tag': 'java?'
+            'question_title': 'Java',
+            'question_body': 'how to write a method using hibernate'
         }
         res = self.app.post(DEFAULT_URL,
                             data=json.dumps(question),
                             content_type='application/json')
         self.assertEqual(res.status_code, 201)
 
-    # def test_question_creation(self):
-    #     """Test API can create a question (POST request)"""
-    #     res = self.app.get(DEFAULT_URL)
-    #     self.assertEqual(res.status_code, 201)
-    #     self.assertIn('Datatypes-boolean', str(res.data))
+    def test_api_bad_request_question(self):
+        question = {
+            'question_title': 'python',
+            'question_body': 'How do I configure the server'
+        }
+        res = self.app.post(DEFAULT_URL,
+                            data=json.dumps(question),
+                            content_type='application/json')
+        self.assertEqual(res.status_code, 400)
 
-    def test_api_can_get_all_questions(self):
-        """Test API can get a question (GET request)."""
-        res = self.app.get(DEFAULT_URL)
-        data = json.loads(res.get_data())
-        self.assertEqual(len(data['questions']), 2)
-        self.assertEqual(res.status_code, 200)
-
-    def test_question_answer(self):
-        res = self.app.get(GOOD_URL)
-        data = json.loads(res.get_data())
-        self.assertEqual(len(data['answers']), 3)
-        self.assertEqual(res.status_code, 200)
- 
     def tearDown(self):
-        """teardown configs after running tests
-            Method to tidy up lists after the test is run
+        """teardown configuratios after running tests
         """
         views.questions = self.questionCopy
         views.answers = self.answerCopy
