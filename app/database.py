c6a9d811ac2ccc55639a03399ff8883e2cc95e4d
Takia5 <kimflower92@gmail.com>
2018-08-28 17:08:09 +0300

    update database

--- app/database.py
+++ app/database.py
@@ -1,18 +1,17 @@
 import psycopg2
-import os
-
 from pprint import pprint
 
 
-class DatabaseConnection():
-    """This class handles postres DB"""
+class DatabaseConnection:
+    """This class handles postgres DB"""
 
     def __init__(self):
         try:
             self.conn = psycopg2.connect(
-                host="localhost", database="stackover", user="postgres", password=" "
+                database='stackover', user='postgres', host='localhost', password='keko', port='5432'
             )
-            self.conn.autocommit = True
+            self.cursor = self.conn.cursor()
+            self.conn.autocommit = True  
             self.cursor = self.conn.cursor()
             print('Connection successful')
         except (Exception, psycopg2.DatabaseError) as e:
@@ -25,3 +24,37 @@ class DatabaseConnection():
         print('db connection closed ')
         self.cursor.close()
         self.conn.close()
+
+    def create_tables(self):
+
+        """Create tables needed"""
+
+        create_tables_command1 = """CREATE TABLE IF NOT EXISTS users(
+             user_id SERIAL PRIMARY KEY,
+             username VARCHAR(100),
+             email VARCHAR(100) NOT NULL, 
+             password VARCHAR(15) NOT NULL
+         );"""
+
+        create_tables_command2 = """CREATE TABLE IF NOT EXISTS questions(
+             question_id SERIAL PRIMARY KEY,
+             title VARCHAR(100),
+             body VARCHAR(255) NOT NULL, 
+             tag VARCHAR(50) NOT NULL
+         );"""
+
+        create_tables_command3 = """CREATE TABLE IF NOT EXISTS answers(
+             answer_id SERIAL PRIMARY KEY,
+             body VARCHAR(100)
+         );"""
+       
+        try:
+           self.cursor.execute(create_tables_command2)
+           self.cursor.execute(create_tables_command3)
+           self.cursor.execute(create_tables_command1)
+        except:
+             pprint('Cant connect to the database')
+
+
+conn = DatabaseConnection()
+conn.create_tables()
